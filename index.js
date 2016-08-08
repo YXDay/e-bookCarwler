9c3fb4d81da9e3338dc632c4817eff5ae0ea3848
thu <yanxu_dai@163.com>
2016-08-08 23:50:24 +0800

    add

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var cheerio = require('cheerio');
+var superagent = require('superagent');
+
+var app = express();
+
+var book = process.argv[2];
+var result = [];
+
+app.get('/', function (req, res, next) {
+  superagent.get('http://s-e.jd.com/Search?key='+book+'&enc=utf-8')
+    .end(function (err, sres) {
+      if (err) {
+        return next(err);
+      }
+      var $ = cheerio.load(sres.text);
+      result.push({
+        'jingdong': ($(".skcolor_ljg").eq(0).text() == book) && (book !== "")
+      });
+      res.send(result);
+    });
+});
+
+
+app.listen(3000, function () {
+  console.log('app is listening at port 3000');
+});
